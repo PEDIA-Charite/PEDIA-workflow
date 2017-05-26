81130f193765fbd7e38bf60ed7c6ae3d11c73dda
Max <max.schubach@googlemail.com>
2017-05-26 18:47:01 +0200

    phenomization script fix
    still really messi. I had to do it again!

--- 2_phenomization/scripts/phenomize_jsons.py
+++ 2_phenomization/scripts/phenomize_jsons.py
@@ -8,6 +8,8 @@ import sys, getopt
 
 argv = sys.argv[1:]
 
+import re
+
 morbidmap = ''
 mimfile = ''
 phenomizerconfig = ''
@@ -48,115 +50,142 @@ config = json.loads(open(phenomizerconfig).read())
 
 
 def load_omim():
-    global mim
-    global omimID2gene
-    mim={}
-    omimID2gene={}
-    for line in open(morbidmap):
-        fields=line[:-1].split('\t')
-        if len(fields)>1:
-            ID=fields[0].split(', ')[-1][:-4]
-            if '}' not in ID:
-                genes=fields[1]
-                genes=genes.split(', ')
-                #print(ID, genes)
-                if ID in omimID2gene:
-                    for gene in genes:
-                        omimID2gene[ID].append(gene)
-                if ID not in omimID2gene:
-                    omimID2gene[ID]=genes
+	global mim
+	global omimID2gene
+	global geneToGeneID
+	geneToGeneID={}
+	mim={}
+	omimID2gene={}
+	for line in open(morbidmap):
+		fields=line[:-1].split('\t')
+		if len(fields)>1:
+			ID=fields[0].split(', ')[-1][:-4]
+			if '}' not in ID:
+				genes=fields[1]
+				genes=genes.split(', ')
+				#print(ID, genes)
+				if ID in omimID2gene:
+				    for gene in genes:
+				        omimID2gene[ID].append(gene)
+				if ID not in omimID2gene:
+				    omimID2gene[ID]=genes
 
-    for line in open(mimfile):
-        if line[0]!='#':
-            fields=line[:-1].split('\t')
-            mimid=fields[0]
-            gene=fields[3]
-            mim[gene]=mimid
+	for line in open(mimfile):
+		if line[0]!='#':
+			fields=line[:-1].split('\t')
+			entry = fields[1]
+			if ((entry == "gene") & (fields[2] != "")):
+				mimid=fields[0]
+				gene=fields[3]
+				gene_id=int(fields[2])
+				mim[gene_id]={"mimid": mimid,"gene": gene}
+				geneToGeneID[gene] = gene_id
 
 
 
 def annotate_pheno_and_boqa():   #load omim first
-    file = inputfile
-    with open(file) as json_data:
-        data = json.load(json_data)
-    data['processing']=['python phenomize_jsons.py']
-    scores={}
-    prefix=config["phenomizer"]
-    suffix="&numres=100"
-    hpo=''
-    for i in data['features']:
-        #print(i)
-        hpo+=(i+',')
-    link=prefix +hpo[:-1] + suffix
-    print(link)
-    content = requests.get(link).text
-    lines=content.split('\n')
-    for line in lines:
-        fields=line.split('\t')
-        if len(fields)>3:
-            phenoscore= 1-float(fields[0])
-            genes=fields[4]
-            if len(genes)>1:
-                genes=genes.split(', ')
-                for i in genes:
-                    if i not in scores:
-                        scores[i]=phenoscore
 
-    for entry in data['geneList']:
-#                if data['geneList'][j]['gene_symbol'] not in scores:
-#                    data['geneList'][j]['pheno_score']=float(0)
-        if entry['gene_symbol'] in scores:
-            entry['pheno_score']=scores[entry['gene_symbol']]
-            scores.pop(entry['gene_symbol'])
-    for i in scores:
-        entry={}
-#                entry['combined_score']=float(0)
-#                entry['feature_score']=float(0)
-        entry['gene_omim_id']='?'
-        if i in mim:
-            entry['gene_omim_id']=mim[i]
-            #print(mim[i])
-        entry['gene_symbol']=i
-#                entry['gestalt_score']=float(0)
-        entry['pheno_score']=scores[i]
-        data['geneList'].append(entry)
-    scores={}
-    prefix=config["boqa"]
-    link=prefix +hpo[:-1] + suffix
-    print(link)
-    content = requests.get(link).text
-    lines=content.split('\n')
-    for line in lines:
-        fields=line.split('\t')
-        if len(fields)>2:
-            boqascore= float(fields[0])
-            Omim=fields[2][10:]
-            if Omim in omimID2gene:
-                genes=omimID2gene[Omim]
-                for i in genes:
-                    if i not in scores:
-                        scores[i]=boqascore
-    for j in range(len(data['geneList'])):
-#                if data['geneList'][j]['gene_symbol'] not in scores:
-#                    data['geneList'][j]['boqa_score']=float(0)
-        if data['geneList'][j]['gene_symbol'] in scores:
-            data['geneList'][j]['boqa_score']=scores[data['geneList'][j]['gene_symbol']]
-            scores.pop(data['geneList'][j]['gene_symbol'])
-    for i in scores:
-        entry={}
-#                entry['combined_score']=float(0)
-#                entry['feature_score']=float(0)
-        entry['gene_omim_id']='?'
-        if i in mim:
-            entry['gene_omim_id']=mim[i]
-        entry['gene_symbol']=i
-#                entry['gestalt_score']=float(0)
-#                entry['pheno_score']=float(0)
-        entry['boqa_score']=scores[i]
-        data['geneList'].append(entry)
 
-    with open(outputfile, 'w') as f:
-         json.dump(data, f)
+
+	file = inputfile
+	with open(file) as json_data:
+	    data = json.load(json_data)
+	data['processing']=['python phenomize_jsons.py']
+
+	#### Get phenomizer
+	gene_pattern = re.compile("(.+) \((\d+)\)")
+	scores={}
+	prefix=config["phenomizer"]
+	suffix="&numres=100"
+	hpo=''
+	for i in data['features']:
+	    #print(i)
+	    hpo+=(i+',')
+	link=prefix +hpo[:-1] + suffix
+	print(link)
+	content = requests.get(link).text
+	lines=content.split('\n')
+	for line in lines:
+		fields=line.split('\t')
+		if len(fields)>3:
+			phenoscore= 1-float(fields[0])
+			genes=fields[4]
+			if len(genes)>1:
+			    genes=genes.split(', ')
+			    for geneterm in genes:
+					gene_id = int(gene_pattern.findall(geneterm)[0][1])
+					gene = str(gene_pattern.findall(geneterm)[0][0])
+					if gene_id not in mim:
+						mim[gene_id]={"mimid": "?","gene": gene}
+					if gene not in geneToGeneID:
+						geneToGeneID[gene]=gene_id
+					if gene_id not in scores:
+						scores[gene_id] = phenoscore
+					else:
+						scores[gene_id]=max(scores[gene_id], phenoscore)
+
+	usedIDs=set()
+	for entry in data['geneList']:
+	#                if data['geneList'][j]['gene_symbol'] not in scores:
+	#                    data['geneList'][j]['pheno_score']=float(0)
+		gene_id = int(entry['gene_id'])
+		if gene_id in scores:
+			entry['pheno_score']=scores[gene_id]
+			usedIDs.add(gene_id)
+	for gene_id in scores:
+		if gene_id in usedIDs:
+			continue
+		entry={}
+		entry['gene_omim_id']=mim[gene_id]['mimid']
+		entry['gene_symbol']=mim[gene_id]['gene']
+		entry['pheno_score']=scores[gene_id]
+		entry['gene_id']=gene_id
+		data['geneList'].append(entry)
+
+	##### Get BOQA
+	scores={}
+	usedIDs = set()
+	prefix=config["boqa"]
+	link=prefix +hpo[:-1] + suffix
+	print(link)
+	content = requests.get(link).text
+	lines=content.split('\n')
+	for line in lines:
+		fields=line.split('\t')
+		if len(fields)>2:
+			boqascore= float(fields[0])
+			Omim=fields[2][10:]
+			if Omim in omimID2gene:
+				genes=omimID2gene[Omim]
+				for gene in genes:
+					if gene not in geneToGeneID:
+						print "cannot get gene_id for gene"+gene
+						continue
+					gene_id = geneToGeneID[gene]
+					if gene_id not in scores:
+						scores[gene_id] = boqascore
+					else:
+						scores[gene_id]=max(scores[gene_id], boqascore)
+	usedIDs=set()
+	for entry in data['geneList']:
+	#                if data['geneList'][j]['gene_symbol'] not in scores:
+	#                    data['geneList'][j]['pheno_score']=float(0)
+		gene_id = int(entry['gene_id'])
+		if gene_id in scores:
+			entry['boqa_score']=scores[gene_id]
+			usedIDs.add(gene_id)
+	for gene_id in scores:
+		if gene_id in usedIDs:
+			continue
+		entry={}
+		entry['gene_omim_id']=mim[gene_id]['mimid']
+		entry['gene_symbol']=mim[gene_id]['gene']
+		entry['boqa_score']=scores[gene_id]
+		entry['gene_id']=gene_id
+		data['geneList'].append(entry)
+
+	with open(outputfile, 'w') as f:
+		json.dump(data, f)
 
 load_omim()
 annotate_pheno_and_boqa()
