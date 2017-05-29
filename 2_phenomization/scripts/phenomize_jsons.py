13cce32773b2da746ddecceb49b5d89a588eab68
Max <max.schubach@googlemail.com>
2017-05-29 10:30:18 +0200

    Phenomization bug fixing
    - fixing bug in reading morbid map
    - fixing bug in getting boqa score. now the orphanet and omim ids are not used as omim id
    - Big refactoring and c mments. so strat making the unreadable code readable

--- 2_phenomization/scripts/phenomize_jsons.py
+++ 2_phenomization/scripts/phenomize_jsons.py
@@ -1,15 +1,25 @@
+
+# JSON, dirs and http requests
 import json, requests, os
 
 
 # GetOpt to read cli inputs
 import sys, getopt
 
-# CLI-Options
-
-argv = sys.argv[1:]
-
+# use regular expressions
 import re
 
+
+
+
+
+
+
+
+
+##### command-line input ####
+
+# CLI-Options
 morbidmap = ''
 mimfile = ''
 phenomizerconfig = ''
@@ -17,16 +27,18 @@ outputfolder = ''
 inputfile = ''
 outputfile = ''
 
+argv = sys.argv[1:]
+
 try:
 	opts, args = getopt.getopt(argv,"h::",["help","mimfile=","morbidmap=","phenomizerconfig=","inputfile=","outputfile="])
 except getopt.GetoptError as e:
     print(e)
-    print('jsonToTable.py --jsonsoriginal --log --login ')
+    print("phenomize_jsons.py --mimfile <mim2gene.txt> --morbidmap <morbidmap.txt> --phenomizerconfig <protected/phenomizer_server.json> --inputfile <input.json> --outputfile <output.json>")
     sys.exit(2)
 
 for opt, arg in opts:
     if opt in ("-h", "--help"):
-        print('jsonToTable.py -i <input-folder> -o <output-file.tsv>')
+        print("phenomize_jsons.py --mimfile <mim2gene.txt> --morbidmap <morbidmap.txt> --phenomizerconfig <protected/phenomizer_server.json> --inputfile <input.json> --outputfile <output.json>")
         sys.exit(1)
     elif opt in ("--morbidmap"):
         morbidmap = arg
@@ -39,70 +51,84 @@ for opt, arg in opts:
     elif opt in ("--inputfile"):
     	inputfile = arg
 
-print 'mimfile:',mimfile
-print 'morbidmap file:',morbidmap
-print 'phenomizerconfig file:',phenomizerconfig
-print 'input file:',inputfile
-print 'output file:',outputfile
+print 'Mimfile:',mimfile
+print 'Morbidmap file:',morbidmap
+print 'Phenomizerconfig file:',phenomizerconfig
+print 'Input file:',inputfile
+print 'Output file:',outputfile
 
+####
+# function to load mim and mobidmap file
+####
 
-config = json.loads(open(phenomizerconfig).read())
 
 
 def load_omim():
+	# gene_id -> {mimid: <id>, gene: <symbol>}
 	global mim
+	# omim_syndrom_id -> [<symbol1>, <symbol2>,...]
 	global omimID2gene
+	# symbol -> gene_id
 	global geneToGeneID
+
+	# init maps
 	geneToGeneID={}
 	mim={}
 	omimID2gene={}
+
+	#load morbidmap and set omimID2gene
+	omim_sydrom_id_pattern = re.compile(".+, (\d+) \(\d+\)")
 	for line in open(morbidmap):
+		if line.startswith("#") or line.startswith("{"): # skip headers or sucessibility
+			continue
 		fields=line[:-1].split('\t')
-		if len(fields)>1:
-			ID=fields[0].split(', ')[-1][:-4]
-			if '}' not in ID:
-				genes=fields[1]
-				genes=genes.split(', ')
-				#print(ID, genes)
-				if ID in omimID2gene:
-				    for gene in genes:
-				        omimID2gene[ID].append(gene)
-				if ID not in omimID2gene:
-				    omimID2gene[ID]=genes
 
+		#if len(fields)>1:
+		search_result = omim_sydrom_id_pattern.findall(fields[0])
+		if len(search_result) > 0:
+			omim_syndrom_id = int(search_result[0])
+			genes=fields[1].split(', ')
+			#print(ID, genes)
+			if omim_syndrom_id in omimID2gene:
+			    for gene in genes:
+			        omimID2gene[omim_syndrom_id].append(gene)
+			if omim_syndrom_id not in omimID2gene:
+			    omimID2gene[omim_syndrom_id]=genes
+
+	# load mimfile and set mim and geneToGeneID
 	for line in open(mimfile):
-		if line[0]!='#':
-			fields=line[:-1].split('\t')
-			entry = fields[1]
-			if ((entry == "gene") & (fields[2] != "")):
-				mimid=fields[0]
-				gene=fields[3]
-				gene_id=int(fields[2])
-				mim[gene_id]={"mimid": mimid,"gene": gene}
-				geneToGeneID[gene] = gene_id
+		if line.startswith("#"): # skip headers
+			continue
+		fields=line[:-1].split('\t')
+		entry = fields[1]
+		if ((entry == "gene") & (fields[2] != "")):
+			mimid=fields[0]
+			gene=fields[3]
+			gene_id=int(fields[2])
+			mim[gene_id]={"mimid": mimid,"gene": gene}
+			geneToGeneID[gene] = gene_id
 
 
+def gethpoIDs(data):
+	return data['features']
 
-def annotate_pheno_and_boqa():   #load omim first
+###################
+# annotate phenomizer
+###################
+
+def annotate_phenomizer(data, url_prefix, url_suffix):   #load omim first
 
 
-
-	file = inputfile
-	with open(file) as json_data:
-	    data = json.load(json_data)
 	data['processing']=['python phenomize_jsons.py']
 
 	#### Get phenomizer
 	gene_pattern = re.compile("(.+) \((\d+)\)")
 	scores={}
-	prefix=config["phenomizer"]
-	suffix="&numres=100"
-	hpo=''
-	for i in data['features']:
-	    #print(i)
-	    hpo+=(i+',')
-	link=prefix +hpo[:-1] + suffix
+	url_hpos =','.join(gethpoIDs(data))
+	link=url_prefix + url_hpos + url_suffix
 	print(link)
+
+	# recieve test
 	content = requests.get(link).text
 	lines=content.split('\n')
 	for line in lines:
@@ -142,30 +168,43 @@ def annotate_pheno_and_boqa():   #load omim first
 		entry['gene_id']=gene_id
 		data['geneList'].append(entry)
 
-	##### Get BOQA
+	return data
+
+
+###################
+# annotate boqa
+###################
+def annotate_boqa(data, url_prefix, url_suffix):   #load omim first
+
+	##### geterate link
 	scores={}
 	usedIDs = set()
-	prefix=config["boqa"]
-	link=prefix +hpo[:-1] + suffix
+	url_hpos = ','.join(gethpoIDs(data));
+	link = url_prefix + url_hpos + url_suffix
 	print(link)
+
+	## get context from link
 	content = requests.get(link).text
 	lines=content.split('\n')
 	for line in lines:
 		fields=line.split('\t')
-		if len(fields)>2:
+		if len(fields)>2: # there could be lines with only 2 entries
 			boqascore= float(fields[0])
-			Omim=fields[2][10:]
-			if Omim in omimID2gene:
-				genes=omimID2gene[Omim]
-				for gene in genes:
-					if gene not in geneToGeneID:
-						print "cannot get gene_id for gene"+gene
-						continue
-					gene_id = geneToGeneID[gene]
-					if gene_id not in scores:
-						scores[gene_id] = boqascore
-					else:
-						scores[gene_id]=max(scores[gene_id], boqascore)
+
+			syndromDB = fields[2].split(":")[0]
+			if (syndromDB == "OMIM"): # skip orphanet
+				omim_syndrom_id = int(fields[2].split(":")[2])
+				if omim_syndrom_id in omimID2gene:
+					genes=omimID2gene[omim_syndrom_id]
+					for gene in genes:
+						if gene not in geneToGeneID:
+							print "cannot get gene_id for gene " + gene
+							continue
+						gene_id = geneToGeneID[gene]
+						if gene_id not in scores:
+							scores[gene_id] = boqascore
+						else:
+							scores[gene_id]=max(scores[gene_id], boqascore)
 	usedIDs=set()
 	for entry in data['geneList']:
 	#                if data['geneList'][j]['gene_symbol'] not in scores:
@@ -184,8 +223,20 @@ def annotate_pheno_and_boqa():   #load omim first
 		entry['gene_id']=gene_id
 		data['geneList'].append(entry)
 
-	with open(outputfile, 'w') as f:
-		json.dump(data, f)
+	return data
 
 load_omim()
-annotate_pheno_and_boqa()
+
+## load config fille to login on phenomizer/boqa
+config = json.loads(open(phenomizerconfig).read())
+
+# load data to input
+with open(inputfile) as json_data:
+	data = json.load(json_data)
+
+data = annotate_phenomizer(data, config['phenomizer'], "&numres=100")
+data = annotate_boqa(data, config['boqa'], "&numres=100")
+
+#write data in pouput
+with open(outputfile, 'w') as f:
+	json.dump(data, f)
