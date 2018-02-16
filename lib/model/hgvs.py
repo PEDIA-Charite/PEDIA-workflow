ba3195321090483dd9a2c84f1256982e9b855f83
Max Zhao <alcasa.mz@gmail.com>
2018-02-16 15:09:27 +0100

    Type hints and improved documentation in the hgvs library. Some unneeded functions have been removed

--- lib/model/hgvs.py
+++ lib/model/hgvs.py
@@ -1,7 +1,6 @@
-from pprint import pprint
-import enum
-import os
-import csv
+'''
+Parser class to process HGVS Information with diverse content.
+'''
 import re
 
 import hgvs
@@ -9,79 +8,89 @@ import hgvs.parser
 import hgvs.validator
 import hgvs.exceptions
 
-from lib.utils import list_all_in
-from lib.error_solver import ErrorFixer
+from lib.errorfixer import ErrorFixer
 from lib.api.mutalyzer import Mutalyzer
 from lib.constants import HGVS_OPS, HGVS_PREFIX
 
-'''
-Parser class to process HGVS Information with diverse content.
-'''
 
-RE_PROTEIN = re.compile('\w \((\w+)\)')
-
-def extract_amino(string):
-    '''Extract the three letter Code from the Face2Gene Amino acid description.
-    They follow the scheme: X (XXX), although nonsense is written as Ter (Stop). So we need to take this into account.
-    '''
-    m = RE_PROTEIN.search(string)
-    m = m and m.group(1) or ''
-    # replace stop, which is not a valid three letter code with Ter
-    m = m == 'Stop' and 'Ter' or m
-    return m
-
-RE_HGVS = re.compile('[gcmnrp]\.\d+', re.IGNORECASE)
-
+# external API for hgvs string checking and RS number resolution
 MUTALYZER = Mutalyzer()
-
+# creation of hgvs objects from hgvs strings
 HGVS_PARSER = hgvs.parser.Parser()
+# validation of created hgvs objects
 HGVS_VALIDATOR = hgvs.validator.IntrinsicValidator()
 
 ERROR_FIXER = ErrorFixer()
 
-def is_hgvs(string):
+# REGEX matching proteins denoted by 'X (Triplet code)'
+RE_PROTEIN = re.compile('\w \((\w+)\)')
+# rough matching of hgvs strings
+RE_HGVS = re.compile('[gcmnrp]\.\d+', re.IGNORECASE)
+# matching of double bracket expressions, such as:
+# xyz(abc):xyz(abc) - this was used by some people to denoted protein and dna
+# sequences
+RE_DOUBLE_BRACKETS = re.compile('([\w_.]+)\([\w_.]+\):([\w_.><]+)\([\w_.>]+\)')
+
+
+def extract_amino(protein_code: str) -> str:
+    '''Extract the three letter Code from the Face2Gene Amino acid description.
+    They follow the scheme: X (XXX), although nonsense is written as Ter
+    (Stop). So we need to take this into account.
+    '''
+    match = RE_PROTEIN.search(protein_code)
+    match = match and match.group(1) or ''
+    # replace stop, which is not a valid three letter code with Ter
+    return match == 'Stop' and 'Ter' or match
+
+
+def is_hgvs(hgvs_candidate: str) -> bool:
     '''Check if a string might be a hgvs code.
     '''
     # remove all whitespace
-    string = "".join(string.split())
-    match = RE_HGVS.search(string)
-    return bool(match)
+    hgvs_candidate = "".join(hgvs_candidate.split())
+    return bool(RE_HGVS.search(hgvs_candidate))
 
-RE_DOUBLE_BRACKETS = re.compile('([\w_.]+)\([\w_.]+\):([\w_.><]+)\([\w_.>]+\)')
 
-def clean_hgvs(string):
+def clean_hgvs(hgvs_string: str) -> str:
     '''Remove extraneous information from possible hgvs code.
     '''
     # remove any whitespace first
-    no_white = "".join(string.split())
+    no_white = "".join(hgvs_string.split())
     # resolve strings of format NORMAL(PROTEIN):NORMAL(PROTEIN)
-    m = RE_DOUBLE_BRACKETS.search(no_white)
-    match = m and ":".join([m.group(1), m.group(2)]) or no_white
-    return match
+    match = RE_DOUBLE_BRACKETS.search(no_white)
+    return match and ":".join([match.group(1), match.group(2)]) or no_white
 
-def get_multi_field(data, candidate_ids):
+
+def get_multi_field(data: dict, candidate_ids: [str]) -> str:
     '''Get information from a number of non-empty fields.
-    Assert that information in these fields do not differ, as to prevent unforeseen erros further down the line.
+    Assert that information in these fields do not differ, as to prevent
+    unforeseen erros further down the line.
     '''
     entry = ''
     for cid in candidate_ids:
-        e = cid in data and data[cid] or ''
-        if e and entry:
-            assert e == entry, '{} and {} are not same'.format(e,entry)
-        elif e:
-            entry = e
+        candidate = cid in data and data[cid] or ''
+        # check if entry already assigned, check if it matches the previous
+        # entry
+        if candidate and entry:
+            assert candidate == entry, '{} and {} are not same'.format(
+                candidate, entry)
+        elif candidate:
+            entry = candidate
     return entry
+
 
 class HGVSModel:
     '''Class to model mutation information received from Face2Gene.'''
 
-    def __init__(self, entry_dict, entry_type='new'):
+    def __init__(self, entry_dict: dict, entry_type: str='new'):
         if entry_type == 'new':
             self._parse_new(entry_dict)
         else:
-            raise TypeError('Only parsing of new genomic entry format has been implemented')
+            raise TypeError(
+                'Only parsing of new genomic entry format has been implemented'
+            )
 
-    def _parse_new(self, entry_dict):
+    def _parse_new(self, entry_dict: dict):
         '''New gene entry format contains:
         entry_id - entry id of gene entry json file
         test_type
@@ -89,16 +98,22 @@ class HGVSModel:
         result
         variant_type
         variants
+
+        See doc/genomic_entry.md for reference on the format of genomic
+        entries.
         '''
         entry_dict = ERROR_FIXER[entry_dict]
         if not entry_dict:
-            print('Skipping empty entry in error dict. It needs to be manually fixed in {} first.'.format(ERROR_FIXER.filename()))
+            print('Skipping empty entry in error dict. It needs to be \
+                  manually fixed in {} first.'.format(ERROR_FIXER.filename()))
             self.variants = []
             return
 
         self.entry_id = entry_dict['entry_id']
-        self.gene = 'gene' in entry_dict and entry_dict['gene'] or {'gene_id' : '', 'gene_symbol' : '', 'gene_omim_id' : ''}
-        self.result = 'result' in entry_dict and entry_dict['result'] or 'UNKNOWN'
+        self.gene = 'gene' in entry_dict and entry_dict['gene'] or {
+            'gene_id': '', 'gene_symbol': '', 'gene_omim_id': ''}
+        self.result = 'result' in entry_dict and entry_dict['result'] or \
+            'UNKNOWN'
         self.test_type = entry_dict['test_type'] or 'UNKNOWN'
         self.variant_type = entry_dict['variant_type'] or 'UNKNOWN'
 
@@ -110,62 +125,74 @@ class HGVSModel:
             ERROR_FIXER.add_faulty(entry_dict, candidates)
             self.variants = []
 
-    def _parse_variants(self, variants):
+    def _parse_variants(self, variant_dict: dict) -> (list, list):
         '''Create variant information from entries in the form of hgvs codes.
-        If information is not parseable, create entry in error dictionary which can be filled by hand.
+        If information is not parseable, create entry in error dictionary which
+        can be filled by hand.
 
-        We initially collect a number of hgvs candidates, which we will try to complete using additional data.
+        We initially collect a number of hgvs candidates, which we will try to
+        complete using additional data.
 
-        Params:
-            variants Dictionary containing all variant information.
+        Args:
+            variants: Dictionary containing all variant information.
 
         Returns:
-            Tuple containing a list of entered hgvs objects and a list of generated hgvs candidate strings.
+            Tuple containing a list of entered hgvs objects and a list of
+            generated hgvs candidate strings.
         '''
-        if not variants:
+        if not variant_dict:
             return [], []
 
+        # candidates are possible hgvs strings, these are collected from
+        # various sources
         hgvs_candidates = []
 
-        variant_information = 'variant_information' in variants and variants['variant_information'] or 'UNKNOWN'
+        variant_information = 'variant_information' in variant_dict \
+            and variant_dict['variant_information'] or 'UNKNOWN'
 
-        hgvs_description = 'hgvs_variant_description' in variants and variants['hgvs_variant_description'] or ''
+        hgvs_description = 'hgvs_variant_description' in variant_dict \
+            and variant_dict['hgvs_variant_description'] or ''
         if is_hgvs(hgvs_description):
             hgvs_candidates.append(hgvs_description)
 
-        notes = 'notes' in variants and variants['notes'] or ''
+        notes = 'notes' in variant_dict and variant_dict['notes'] or ''
         if is_hgvs(notes):
             hgvs_candidates.append(notes)
 
-        mutations = self._get_mutations(variants)
+        mutations = self._get_mutations(variant_dict)
         for mutation in mutations:
-            hgvs_candidates += self._parse_mutations(mutation, variant_information)
+            hgvs_candidates += self._parse_mutations(
+                mutation, variant_information)
         variants = []
-        for cand in hgvs_candidates:
-            hg = clean_hgvs(cand)
-            if hg:
+
+        # try to parse collected hgvs strings
+        # only return successfully parsed hgvs strings
+        for candidate in hgvs_candidates:
+            cleaned_hgvs = clean_hgvs(candidate)
+            if cleaned_hgvs:
                 try:
-                    var = HGVS_PARSER.parse_hgvs_variant(hg)
+                    var = HGVS_PARSER.parse_hgvs_variant(cleaned_hgvs)
                     variants.append(var)
                 except hgvs.exceptions.HGVSParseError:
-                    print('{}: Error parsing {}, uncleaned {}'.format(self.entry_id, hg, cand))
+                    print('{}: Error parsing {}, uncleaned {}'.format(
+                        self.entry_id, cleaned_hgvs, candidate))
         return variants, hgvs_candidates
 
-
-    def _get_mutations(self, data):
+    def _get_mutations(self, data: dict) -> [dict]:
         '''Get mutation information from mutation fields.
         '''
         if 'mutation' in data:
             mutations = [data['mutation']]
-            mutations = [ x for x in mutations if x ]
+            mutations = [x for x in mutations if x]
         elif 'mutation1' in data:
-            mutations = [data['mutation1'],data['mutation2']]
+            mutations = [data['mutation1'], data['mutation2']]
         else:
             mutations = {}
-        mutations = [ x for x in mutations if x ]
+        mutations = [x for x in mutations if x]
         return mutations
 
-    def _parse_mutations(self, mutation, variant_information):
+    def _parse_mutations(self, mutation: dict, variant_information: str) \
+            -> [str]:
         '''Get hgvs candidate strings from mutation information.
         '''
         candidates = []
@@ -173,36 +200,45 @@ class HGVSModel:
         transcript = 'transcript' in mutation and mutation['transcript'] or ''
         if is_hgvs(transcript):
             candidates.append(transcript)
-            # if we a real transcript part, we should be able to split it off
-            s = transcript.split(':')[0]
-            if not is_hgvs(s):
-                transcript = s
+            # if we have a real transcript part, we should be able to split it
+            # off
+            possible = transcript.split(':')[0]
+            # if it doesnt contain variant patterns, we use it
+            if not is_hgvs(possible):
+                transcript = possible
             else:
                 transcript = ''
 
         rs_number = 'rs_number' in mutation and mutation['rs_number'] or ''
         if rs_number:
             j = MUTALYZER.getdbSNPDescriptions(rs_number)
-            # add the first entry, since we will have a much too large number of entries
+            # add the first entry, since we will have a much too large number
+            # of entries
             if j:
                 candidates.append(j[0])
 
         # might be something like substitution, deletion etc
-        mutation_type = 'mutation_type' in mutation and mutation['mutation_type'] or 'UNKNOWN'
+        mutation_type = 'mutation_type' in mutation \
+            and mutation['mutation_type'] or 'UNKNOWN'
 
         # get location information
-        location_ids = ['location', 'first_amino_position', 'last_amino_position']
         location = 'location' in mutation and mutation['location'] or ''
-        # we are given protein codes in format SINGLE_CODE (TRIPLET CODE), since HGVS uses three letter codes
-        p1_loc = 'first_amino_position' in mutation and mutation['first_amino_position'] or ''
-        p2_loc = 'last_amino_position' in mutation and mutation['last_amino_position'] or ''
+        # we are given protein codes in format SINGLE_CODE (TRIPLET CODE),
+        # since HGVS uses three letter codes
+        p1_loc = 'first_amino_position' in mutation \
+            and mutation['first_amino_position'] or ''
+        p2_loc = 'last_amino_position' in mutation \
+            and mutation['last_amino_position'] or ''
         if p1_loc or p2_loc:
             protein = True
 
         # get entry1 and entry2
-        entry1_ids = ['first_amino_acid','original_base']
+        entry1_ids = ['first_amino_acid', 'original_base']
         entry1 = get_multi_field(mutation, entry1_ids)
-        entry2_ids = ['last_amino_acid', 'substituted_base','inserted_bases', 'deleted_bases']
+        entry2_ids = [
+            'last_amino_acid', 'substituted_base',
+            'inserted_bases', 'deleted_bases'
+        ]
         entry2 = get_multi_field(mutation, entry2_ids)
 
         if protein:
@@ -227,13 +263,21 @@ class HGVSModel:
 
         return candidates
 
-    def _build_hgvs(self, transcript='', prefix='', position='', orig='', operation='', sub=''):
-        '''Create hgvs variant string from provided information in these fields.
-        This function only fills the provided strings into the fields of interest. It does not try
-        to infer any additional information. The created hgvs strings do not have to be correct. They should
-        be validated using an additional validator.
+    def _build_hgvs(self,
+                    transcript: str='',
+                    prefix: str='',
+                    position: str='',
+                    orig: str='',
+                    operation: str='',
+                    sub: str='') -> str:
+        '''Create hgvs variant string from provided information in these
+        fields.  This function only fills the provided strings into the fields
+        of interest. It does not try to infer any additional information. The
+        created hgvs strings do not have to be correct. They should be
+        validated using an additional validator.
         '''
-        hgvs_string = "{transcript}:{prefix}.{position}{orig}{operation}{sub}".format(
+        hgvs_string = "{transcript}:{prefix}.{position}{orig}{operation}\
+            {sub}".format(
                 transcript=transcript,
                 prefix=prefix,
                 position=position,
@@ -243,7 +287,21 @@ class HGVSModel:
         hgvs_string = "".join(hgvs_string.split())
         return hgvs_string
 
-    def _build_protein_hgvs(self, transcript='', prefix='', position1='', position2='', orig='', operation='', sub=''):
+    def _build_protein_hgvs(
+            self,
+            transcript: str='',
+            prefix: str='',
+            position1: str='',
+            position2: str='',
+            orig: str='',
+            operation: str='',
+            sub: str='') -> str:
+        '''Create hgvs string for proteins. Since location notation in protein
+        is slightly different another tempate has to be used.
+        ---
+        This function is currently quite bloated. It should be possible to make
+        it easier.
+        '''
         orig = extract_amino(orig)
         sub = extract_amino(sub)
         if operation == '>':
@@ -252,16 +310,19 @@ class HGVSModel:
             position2 = position2 and position2 or position1
             assert position1 == position2, 'Position 1 and 2 are not equal'
             position2 = ''
-            hgvs_temp = "{transcript}:{prefix}.{orig}{position1}{position2}{sub}"
+            hgvs_temp = "{transcript}:{prefix}.{orig}{position1}\
+                {position2}{sub}"
         else:
             if orig and sub:
-                hgvs_temp = "{transcript}:{prefix}.{orig}{position1}_{sub}{position2}{operation}"
+                hgvs_temp = "{transcript}:{prefix}.{orig}{position1}_{sub}\
+                    {position2}{operation}"
             else:
                 position1 = position1 and position1 or position2
                 position2 = position2 and position2 or position1
                 assert position1 == position2, 'Position 1 and 2 are not equal'
                 position2 = ''
-                hgvs_temp = "{transcript}:{prefix}.{orig}{sub}{position1}{position2}{operation}"
+                hgvs_temp = "{transcript}:{prefix}.{orig}{sub}{position1}\
+                    {position2}{operation}"
         hgvs_string = hgvs_temp.format(
                 transcript=transcript,
                 prefix=prefix,
