a3bf91a3341d820c8a716eb4f586d586be3c1442
Max Zhao <alcasa.mz@gmail.com>
2018-02-16 13:05:14 +0100

    Simpliying the case model and adding documentation

--- lib/model/case.py
+++ lib/model/case.py
@@ -1,9 +1,10 @@
-from lib.model.json import OldJson, NewJson
-from lib.model.syndrome import Gene, Syndrome
-
 '''
 Case model created from json files.
 '''
+from functools import reduce
+from typing import Union
+from lib.model.json import OldJson, NewJson
+
 
 class Case:
     '''
@@ -17,18 +18,12 @@ class Case:
     vcf - list of vcf filenames
     '''
 
-    def __init__(self, json_object):
-        if isinstance(json_object, OldJson):
-            self._from_old_json(json_object)
-        elif isinstance(json_object, NewJson):
-            self._from_new_json(json_object)
-        else:
-            raise TypeError('Unsupported Type')
+    def __init__(self, json_object: Union[OldJson, NewJson]):
+        self._from_json_object(json_object)
 
-    def _from_old_json(self, data):
-        raise RuntimeError('This has not been implemented')
-
-    def _from_new_json(self, data):
+    def _from_json_object(self, data: Union[OldJson, NewJson]):
+        '''Load case information from json object.
+        '''
         self.case_id = data.get_case_id()
         self.variants = data.get_variants()
         self.syndromes = data.get_syndrome_suggestions_and_diagnosis()
@@ -36,45 +31,52 @@ class Case:
         self.submitter = data.get_submitter()
         self.vcf = data.get_vcf()
 
-    def syndromes_to_genes(self, omim):
-        return self.syndromes.to_genes(omim)
-
-    def phenomize(self, pheno):
+    def phenomize(self, pheno: 'PhenomizerService') -> bool:
         '''Add phenomization information to genes from boqa and phenomizer.
         Args:
-            omim - Omim object to handle id translation.
-            pheno - PhenomizerService to handle API calls for phenomizer and boqa.
+            omim: Omim object to handle id translation.
+            pheno: PhenomizerService to handle API calls for phenomizer and
+                   boqa.
         '''
         pheno_boqa = pheno.disease_boqa_phenomize(self.features)
         if pheno_boqa is None:
             return False
         pheno_boqa.index = pheno_boqa.index.astype(int)
-        r = self.syndromes.merge(pheno_boqa, left_on='omim_id', how='outer', right_index=True)
-        self.syndromes = r
+        # merge pheno and boqa scores dataframe with our current syndromes
+        # dataframe which contains face2gene scores
+        self.syndromes = self.syndromes.merge(
+            pheno_boqa, left_on='omim_id', how='outer', right_index=True)
         return True
 
-    def check(self):
+    def check(self) -> bool:
         '''Check whether Case fulfills all provided criteria.
 
         The criteria are:
-            picture has been provided - gestalt_score in detected_syndromes should be greater than 0
+            picture has been provided - gestalt_score in detected_syndromes
+            should be greater than 0
             clinical diagnosis - selected_syndromes should not be empty
-            single monogenetic disease - not multiple syndromes selected and not multiple pathogenic mutations in different genes
+            single monogenetic disease - not multiple syndromes selected and
+            not multiple pathogenic mutations in different genes
             SNP mutations - no microdel/dup or other large scale aberrations
 
-        Note that this function is more definitive than the json level check, as the validity of hgvs parsing has already been established.
+        Note that this function is more definitive than the json level check,
+        as the validity of hgvs parsing has already been established.
         '''
         valid = True
         issues = []
         # check maximum gestalt score
-        max_gestalt_score = functools.reduce(lambda x,y: max(x,y.gestalt_score), self.detected_syndromes, 0.0)
+        max_gestalt_score = reduce(
+            lambda x, y: max(x, y.gestalt_score), self.detected_syndromes, 0.0)
         if max_gestalt_score <= 0:
-            issues.append('Maximum gestalt score is 0. Probably no image has been provided.')
+            issues.append('Maximum gestalt score is 0. \
+                          Probably no image has been provided.')
             valid = False
 
         # check that only one syndrome has been selected
         if len(self.diagnosis) != 1:
-            issues.append('{} syndromes have been selected. Only 1 syndrome should be selected for PEDIA inclusion.'.format(len(self.diagnosis)))
+            issues.append(
+                '{} syndromes have been selected. Only 1 syndrome should be \
+                selected for PEDIA inclusion.'.format(len(self.diagnosis)))
             valid = False
 
         # check that molecular information is available at all
@@ -84,7 +86,7 @@ class Case:
 
         return valid, issues
 
-    def eligible_training(self):
+    def eligible_training(self) -> bool:
         '''Eligibility of case for training.
         Exclusion criteria are:
             available real vcf
