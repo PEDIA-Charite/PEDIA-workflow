77ce26801a478a36c731c31b4181dcfac13a42a0
Max Zhao <alcasa.mz@gmail.com>
2018-02-16 12:11:32 +0100

    Adding type hints and docstrings to mutalyzer API

--- lib/api/mutalyzer.py
+++ lib/api/mutalyzer.py
@@ -1,25 +1,40 @@
+'''
+Bindings for Mutalyzer. A service which can be used to look up RS numbers and
+check the validity of hgvs strings.
+'''
 import requests
 
+
 class Mutalyzer(requests.Session):
+    '''Implements API bindings for the Mutalyzer.
+    '''
 
     base_url = 'https://mutalyzer.nl/json/'
 
     def __init__(self):
         super().__init__()
 
-    def _call_service(self, method, params):
+    def _call_service(self, method: str, params: dict) -> list:
+        '''Call the Mutalyzer API service.
+        '''
         url = self.base_url + method
-        r = self.get(url, params=params)
-        r.raise_for_status()
-        return r.json()
+        response = self.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
 
-    def getdbSNPDescriptions(self, rs_id):
-        params = {'rs_id' : rs_id}
+    def getdbSNPDescriptions(self, rs_id: str) -> [str]:
+        '''Return a list of possible RS numbers for the given RS code.
+        '''
+        params = {'rs_id': rs_id}
         return self._call_service('getdbSNPDescriptions', params)
 
+
 def main():
-    m = Mutalyzer()
-    j = m.getdbSNPDescriptions("rs386834107")
+    '''Only for testing purposes.
+    '''
+    mut = Mutalyzer()
+    hgvs_list = mut.getdbSNPDescriptions("rs386834107")
+    print(hgvs_list)
 
 
 if __name__ == '__main__':
