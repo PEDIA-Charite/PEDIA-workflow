091b411eff3a37c6f4be9fec98eef614d68249c2
Max Zhao <alcasa.mz@gmail.com>
2018-02-18 09:40:43 +0100

    Current script for generation of old json files
    This file contains all necessary steps for the current generation of old
    json format files. This file can and should be adapted to ones specific
    needs. Any complex functionality, should if possible be put into the
    library itself and not in these scripts.
    
    All steps from reading the input directory are performed, some
    checkpoints are saved as pickles to recover in case of remote api
    failures (frequency might depend on ones internet connection). Print
    statements are mainly used as graphical indicators of progress.

--- newjson_to_oldjson.py
+++ newjson_to_oldjson.py
@@ -11,6 +11,7 @@ import pickle
 # from lib import download
 from lib.api.phenomizer import PhenomizerService
 from lib.api.omim import Omim
+from lib.api.mutalyzer import correct_reference_transcripts
 
 from lib.model.json import NewJson, OldJson
 from lib.model.case import Case
@@ -32,37 +33,39 @@ def main():
     # download.backup_s3_folder(config=config)
 
     # Initial Quality check of new json
-    # unprocessed_jsons = os.path.join(config.aws['download_location'], 'cases')
-    # json_files = [os.path.join(unprocessed_jsons, x)
-    #               for x in os.listdir(unprocessed_jsons)
-    #               if os.path.splitext(x)[1] == '.json']
+    unprocessed_jsons = os.path.join(config.aws['download_location'], 'cases')
+    json_files = [os.path.join(unprocessed_jsons, x)
+                  for x in os.listdir(unprocessed_jsons)
+                  if os.path.splitext(x)[1] == '.json']
 
-    # # corrected is a directory which can contain manually edited case jsons
-    # # that should differ from the original only in content, not in overall
-    # # structure.  this should make resolving some exotic errors a lot easier
-    # corrected = config.preprocess['corrected_location']
-    # new_json_objs = [NewJson.from_file(f, corrected) for f in json_files]
+    # corrected is a directory which can contain manually edited case jsons
+    # that should differ from the original only in content, not in overall
+    # structure.  this should make resolving some exotic errors a lot easier
+    corrected = config.preprocess['corrected_location']
+    new_json_objs = [NewJson.from_file(f, corrected) for f in json_files]
 
-    # # print('Unfiltered', len(new_json_objs))
+    # print('Unfiltered', len(new_json_objs))
 
-    # filtered_new = [j for j in new_json_objs if j.check()[0]]
+    filtered_new = [j for j in new_json_objs if j.check()[0]]
 
-    # print('Filtered rough criteria', len(filtered_new))
+    print('Filtered rough criteria', len(filtered_new))
 
-    # case_objs = [Case(j) for j in filtered_new]
-    # case_objs = [c for c in case_objs if c.variants]
-    # print('Cases with created hgvs objects', len(case_objs))
-    # pickle.dump(case_objs, open('case_cleaned.p', 'wb'))
+    case_objs = [Case(j) for j in filtered_new]
+    case_objs = [c for c in case_objs if c.variants]
+    print('Cases with created hgvs objects', len(case_objs))
+    # batch call mutalyzer
+    correct_reference_transcripts(case_objs)
+    pickle.dump(case_objs, open('case_cleaned.p', 'wb'))
 
     # case_objs[0].phenomize(phen)
     # print(case_objs[0].syndromes)
     # case_objs = pickle.load(open('case_cleaned.p', 'rb'))
 
-    # for case in case_objs:
-    #     print('Phenomizing', case.case_id)
-    #     case.phenomize(phen)
-    # pickle.dump(case_objs, open('case_phenomized.p', 'wb'))
-    case_objs = pickle.load(open('case_phenomized.p', 'rb'))
+    for case in case_objs:
+        print('Phenomizing', case.case_id)
+        case.phenomize(phen)
+    pickle.dump(case_objs, open('case_phenomized.p', 'wb'))
+    # case_objs = pickle.load(open('case_phenomized.p', 'rb'))
     for case in case_objs:
         print('Converting to old json', case.case_id)
         old_js = OldJson.from_case_object(case, 'convert', omim)
