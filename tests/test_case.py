b0b15b6c29798d28ee328d9bfdb73ab984d984b3
Max Zhao <alcasa.mz@gmail.com>
2018-04-14 18:52:14 +0200

    Adding test in case check for single diagnosis with multiple omims

--- tests/test_case.py
+++ tests/test_case.py
@@ -4,20 +4,39 @@ Test case functions
 import os
 import unittest
 from lib.model import json, case
+from tests.test_json_loading import BaseMapping
 
 
-INPUT_PATH = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "data")
-
-
-class CaseTest(unittest.TestCase):
+class CaseTest(BaseMapping):
     '''Case method tests.'''
 
     def setUp(self):
-        input_file = os.path.join(INPUT_PATH, "cases", "normal.json")
-        loaded_correct = json.NewJson.from_file(input_file)
-        self.case = case.Case(loaded_correct)
+        pass
+
+    def load_json(self, name: str) -> dict:
+        loaded = json.NewJson.from_file(
+            self.get_case_path(name)
+        )
+        return loaded
+
+    def load_case(self, name: str) -> case.Case:
+        case_obj = case.Case(
+            self.load_json(name),
+            error_fixer=self.error_fixer
+        )
+        return case_obj
 
     def test_check(self):
-        self.assertTrue(self.case.check(), "Internal self-check did not pass.")
+        tcase = self.load_case("normal.json")
+        check_status, _ = tcase.check(self.omim)
+        self.assertTrue(check_status, "Internal self-check did not pass.")
+
+    def test_multi_omim_single_syndrome(self):
+        '''Assert that single diagnosis with many omim passes check.'''
+        tcase = self.load_case("diagnosis_many_omim.json")
+        check_status, check_data = tcase.check(self.omim)
+        print(check_data)
+        self.assertTrue(
+            check_status,
+            "Single Diagnosis with multiple OMIM ids did not pass."
+        )
