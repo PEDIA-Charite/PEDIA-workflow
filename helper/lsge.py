e37c109c477a104993730e8a2711429cd5ed419d
Max Zhao <alcasa.mz@gmail.com>
2018-05-08 16:18:29 +0200

    Print genomic entry with helper script.

--- helper/lsge.py
+++ helper/lsge.py
@@ -1,9 +1,26 @@
 #!/bin/env python3
+import os
 import json
 import sys
+from pprint import pprint
 
-with open(sys.argv[1], "r") as jsfile:
+jspath = sys.argv[1]
+
+case_path, filename = os.path.split(jspath)
+
+base_path, _ = os.path.split(case_path)
+
+
+with open(jspath, "r") as jsfile:
     data = json.load(jsfile)
 
 for e in data["genomic_entries"]:
     print(e)
+
+    ge_path = os.path.join(base_path, "genomics_entries", "{}.json".format(e))
+    if os.path.exists(ge_path):
+        with open(ge_path) as gefile:
+            ge_data = json.load(gefile)
+        pprint(ge_data)
+    else:
+        print("Could not find entry in path: {}".format(ge_path))
