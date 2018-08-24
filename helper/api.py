8a315cf35bd205e6eb7cdc58d7c0d03516227056
Tzung-Chien <la60312@gmail.com>
2018-08-24 19:05:27 +0200

    Update report script

--- helper/api.py
+++ helper/api.py
@@ -22,7 +22,7 @@ def convert_old(content, omim_dict):
         selected_syn['diagnosis'] = syn['diagnosis']
         selected_syn['syndrome_name'] = syn['syndrome']['syndrome_name']
         selected_syn['omim_id'] = syn['syndrome']['omim_id']
-        selected_syn['has_mask'] = 1
+        selected_syn['has_mask'] = 0
         selected_syns.append(selected_syn)
     data['selected_syndromes'] = selected_syns
 
@@ -35,17 +35,28 @@ def convert_old(content, omim_dict):
     # detected syndromes
     detected_syns = []
     for syn in case_data['suggested_syndromes']:
+        if 'syndrome' not in syn:
+            continue
         detected_syn = {}
         detected_syn['syndrome_name'] = syn['syndrome']['syndrome_name']
+        if syn['syndrome']['omim_id'] == None:
+            continue
         if detected_syn['syndrome_name'] in omim_dict:
             detected_syn['omim_id'] = omim_dict[detected_syn['syndrome_name']]
         else:
-            detected_syn['omim_id'] = syn['syndrome']['omim_id']
+            if len(syn['syndrome']['omim_ids']) == 0:
+                if syn['syndrome']['omim_id'] == 0 or syn['syndrome']['omim_id'] == '0':
+                    continue
+                detected_syn['omim_id'] = syn['syndrome']['omim_id']
+            else:
+                detected_syn['omim_id'] = [omim for omim in syn['syndrome']['omim_ids'] if omim != 0]
+
         detected_syn['combined_score'] = 0
         detected_syn['gestalt_score'] = float(syn['gestalt_score'])
         detected_syn['feature_score'] = float(syn['feature_score'])
-        detected_syn['has_mask'] = 1
+        detected_syn['has_mask'] = syn['syndrome']['app_valid'] 
         detected_syns.append(detected_syn)
+
     data['detected_syndromes'] = detected_syns
     data['algo_deploy_version'] = case_data['algo_version']
     data['lab_info'] = content['lab_info']
@@ -71,6 +82,7 @@ parser = argparse.ArgumentParser(description='Get case json file from lab')
 parser.add_argument('-l', '--lab', help='Select lab number')
 parser.add_argument('-c', '--case', help='Select case id')
 parser.add_argument('-o', '--output', help='Output folder')
+parser.add_argument('-m', '--mapping', help='Omim mapping file 216_gestalt_syn_to_omim_final.csv')
 
 args = parser.parse_args()
 lab = args.lab
@@ -102,12 +114,12 @@ if response.status_code == 200:
 else:
     print(response.status_code)
 
-omim_filename = "gestalt_syn_to_omim.csv"
+omim_filename = args.mapping
 omim_file = open(omim_filename, 'r')
 reader = csv.reader(omim_file, delimiter='\t')
 omim_dict = {}
 for row in reader:
-    if row[1] == "":
+    if row[1] == "[]":
         continue
     omim = row[1][1:-1].split(', ')
     omim_dict[row[0]] = omim 
