b850b52a1582d61094c56ec66c455a392d28a709
Tzung-Chien <la60312@gmail.com>
2019-05-24 19:49:41 +0200

    Fix bug in convert file

--- helper/convert.py
+++ helper/convert.py
@@ -15,17 +15,32 @@ def convert_old(content, omim_dict):
     selected_syns = []
     for syn in case_data["selected_syndromes"]:
         selected_syn = {}
-        selected_syn['diagnosis'] = syn['diagnosis']
+        if 'syndrome' not in syn:
+            continue
+        if syn['syndrome']['omim_id'] == None and len(syn['syndrome']['omim_ids']) == 0:
+            continue
         selected_syn['syndrome_name'] = syn['syndrome']['syndrome_name']
-        selected_syn['omim_id'] = syn['syndrome']['omim_id']
-        selected_syn['has_mask'] = 0
+        if selected_syn['syndrome_name'] in omim_dict:
+            correct = omim_dict[selected_syn['syndrome_name']]
+            selected_syn['omim_id'] = omim_dict[selected_syn['syndrome_name']]
+        else:
+            if len(syn['syndrome']['omim_ids']) == 0:
+                if syn['syndrome']['omim_id'] == 0 or syn['syndrome']['omim_id'] == '0':
+                    continue
+                selected_syn['omim_id'] = syn['syndrome']['omim_id']
+            else:
+                selected_syn['omim_id'] = [omim for omim in syn['syndrome']['omim_ids'] if omim != 0]
+        selected_syn['diagnosis'] = syn['diagnosis']
+        selected_syn['has_mask'] = syn['syndrome']['app_valid']
         selected_syns.append(selected_syn)
     data['selected_syndromes'] = selected_syns
 
     # features
     features = []
     for feature in case_data['selected_features']:
-        features.append(feature['feature']['hpo_full_id'])
+        if feature['is_present'] == '1':
+            features.append(feature['feature']['hpo_full_id'])
+
     data['features'] = features
 
     # detected syndromes
@@ -35,7 +50,7 @@ def convert_old(content, omim_dict):
             continue
         detected_syn = {}
         detected_syn['syndrome_name'] = syn['syndrome']['syndrome_name']
-        if syn['syndrome']['omim_id'] == None:
+        if syn['syndrome']['omim_id'] == None and len(syn['syndrome']['omim_ids']) == 0:
             continue
         if detected_syn['syndrome_name'] in omim_dict:
             detected_syn['omim_id'] = omim_dict[detected_syn['syndrome_name']]
@@ -55,11 +70,15 @@ def convert_old(content, omim_dict):
 
     data['detected_syndromes'] = detected_syns
     data['algo_deploy_version'] = case_data['algo_version']
+    #if 'genomic_entries' in case_data:
+    #    data['genomic_entries'] = case_data['genomic_entries']
+    #else:
     data['genomic_entries'] = []
+
     sub = {}
-    sub['user_email'] = "demo@pedia-study.org"#case_data['posting_user']['userEmail']
-    sub['user_name'] = "pedia@study"#case_data['posting_user']['userDisplayName']
-    sub['user_team'] = "pedia"#case_data['posting_user']['userInstitution']
+    sub['user_email'] = case_data['posting_user']['userEmail']
+    sub['user_name'] = case_data['posting_user']['userDisplayName']
+    sub['user_team'] = case_data['posting_user']['userInstitution']
 
     data['submitter'] = sub
     data['documents'] = [] if 'documents' not in content else content['documents']
